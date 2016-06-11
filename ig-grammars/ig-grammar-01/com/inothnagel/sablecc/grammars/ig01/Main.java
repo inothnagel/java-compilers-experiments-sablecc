7ca586f379ce46172309edef63bd180d7ffa2e1d
Ignatius Nothnagel <inothnagel@gmail.com>
2016-06-11 16:18:35 +0200

    Generate grammar

--- ig-grammars/ig-grammar-01/com/inothnagel/sablecc/grammars/ig01/Main.java
+++ ig-grammars/ig-grammar-01/com/inothnagel/sablecc/grammars/ig01/Main.java
@@ -1,40 +1,37 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.inothnagel.sablecc.grammars.ig01;
 
-import com.inothnagel.sablecc.grammars.ig01.lexer.Lexer;
-import com.inothnagel.sablecc.grammars.ig01.lexer.LexerException;
-import com.inothnagel.sablecc.grammars.ig01.node.Start;
-import com.inothnagel.sablecc.grammars.ig01.parser.Parser;
-import com.inothnagel.sablecc.grammars.ig01.parser.ParserException;
+import com.inothnagel.sablecc.grammars.ig01.generated.lexer.Lexer;
+import com.inothnagel.sablecc.grammars.ig01.generated.lexer.LexerException;
+import com.inothnagel.sablecc.grammars.ig01.generated.node.Start;
+import com.inothnagel.sablecc.grammars.ig01.generated.parser.Parser;
+import com.inothnagel.sablecc.grammars.ig01.generated.parser.ParserException;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PushbackReader;
 
-/**
- * Created by inothnagel on 2016/06/11.
- */
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
         try {
             parse();
-        }  catch(Exception e)
-        {
-            System.out.println(e.getMessage());
+        } catch (Exception var2) {
+            System.out.println(var2.getMessage());
         }
 
     }
 
     public static void parse() throws ParserException, IOException, LexerException {
         System.out.println("Hello!");
-
-        Parser p =
-                new Parser(
-                        new Lexer(
-                                new PushbackReader(
-                                        new InputStreamReader(System.in), 1024)));
-
+        Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(System.in), 1024)));
         Start tree = p.parse();
         System.out.println("tree: " + tree);
-//        tree.apply(new Translation());
     }
 }
